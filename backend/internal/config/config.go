10d5597fbe8d56e7ee47c556b1aa71d6a4af6439
krish-32 <krishnasri9123@gmail.com>
2026-09-15 20:27:45 +0530

    feat: add render api and ensure port configuration starts with a colon

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -11,6 +11,8 @@ func Load() *Config {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = ":8080"
+	} else if port[0] != ':' {
+		port = ":" + port
 	}
 	return &Config{
 		DBPath: "backend.db",
